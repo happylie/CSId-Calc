5c44f86a488cf477065678d9885a572c64fede01
happylie <happylie@hanmail.net>
2022-07-02 15:53:05 +0900

    Update

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Check Session ID Entropy
-Check Session ID Entropy Tool
+# Check Session ID Entropy Calculator(CSId-Calc)
+<div>
+<img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhappylie%2FCSId-Calc&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=view&edge_flat=false"/>
+<img src="https://img.shields.io/badge/Python->=3.5-blue?logo=python&logoColor=white" />
+</div>
+
+## Check Session ID Entropy Calculator
+- https://happylie.tistory.com/145
+
+### 설치 방법
+1. Git Clone
+```
+$ git clone https://github.com/happylie/CSId-Calc.git
+```
+
+### 실행 방법
+1. Help
+```
+$ python csid.py -h       
+usage: Check Session ID Entropy Bit [-h] [-s SID] [-v]
+
+Check Session ID Entropy Bit
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SID, --session SID
+                        Session ID
+  -v, --version         show program's version number and exit
+```
+2. Session ID Entropy Check
+```
+$ python csid.py -s a00a0a1001100a000a0aaaa1aa0000aaa00a0a1001100a000a0aaaa1aa0000aa
+### Session ID Check Entropy ###
++ Session ID : a00a0a1001100a000a0aaaa1aa0000aaa00a0a1001100a000a0aaaa1aa0000aa
++ String Length : 64
++ Characters : 3 Type
++ Strength : 101.4 Bit
++ Result : Vulnerable(At least 128 Bits)
+
+
+$  python csid.py -s 1a44f079183c8492d55805ef18f1079b0357c8d6                                                                
+### Session ID Check Entropy ###
++ Session ID : 1a44f079183c8492d55805ef18f1079b0357c8d6
++ String Length : 40
++ Characters : 16 Type
++ Strength : 160.0 Bit
++ Result : Good
+```
+3. 결과값 설명
+- Session ID : 입력한 Session ID(세션 ID)
+- String Length :  Session ID 값의 길이
+- Characters : Session ID 값의 조합된 글자수
+- Strength : Session ID 의 강도
+    - 128 Bits 이상이 안전
+- Result : 결과( Vulnerable / Good )
+
+### 참고문서
+- https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html
--- csid.py
+++ csid.py
@@ -27,38 +27,36 @@ class CheckEntropy:
         sid_char_len = len(sid)
         sid_len = len(list(set(list(sid))))
         sid_strength = round(math.log2(sid_len ** sid_char_len), 1)
-        sid_result = Colors.GREEN+"Good"+Colors.RESET
+        sid_result = Colors.GREEN+'Good'+Colors.RESET
         if int(sid_strength) < 128:
-            sid_result = Colors.RED+"Vulnerable"+Colors.RESET
-        return sid_len, sid_char_len, sid_strength, sid_result
+            sid_result = Colors.RED+'Vulnerable(At least 128 Bits)'+Colors.RESET
+        return sid_char_len, sid_len, sid_strength, sid_result
 
     def get_check(self):
         try:
             data = self.__get_check_entropy()
-            ret = """
-            + Session ID Check Entropy + 
-            Session ID : {sid}
-            String Length : {sid_len}
-            Characters : {sid_char_len} Type
-            Strength : {sid_strength} Bit
-            Result : {sid_result}
-            """.format(sid=self.session_id, sid_len=data[1], sid_char_len=data[0], sid_strength=data[2], sid_result=data[3])
-            return ret
+            print('### Session ID Check Entropy ###')
+            print('+ Session ID : {sid}'.format(sid=self.session_id))
+            print('+ String Length : {sid_len}'.format(sid_len=data[0]))
+            print('+ Characters : {sid_char_len} Type'.format(sid_char_len=data[1]))
+            print('+ Strength : {sid_strength} Bits'.format(sid_strength=data[2]))
+            print('+ Result : {sid_result}'.format(sid_result=data[3]))
         except Exception as err:
             print("Check Error:: {err}".format(err=err))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='Check Session ID Entropy Bit', description='Check Session ID Entropy Bit')
+    parser = argparse.ArgumentParser(prog='Check Session ID Entropy Calculator',
+                                     description='Check Session ID Entropy Calculator')
     try:
         parser.add_argument('-s', '--session', dest='sid', type=str, help='Session ID')
         parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
         args = parser.parse_args()
         if not args.sid:
-            print("Input Session ID")
+            print('Input Session ID Value')
             sys.exit(0)
         ce = CheckEntropy(args.sid)
-        print(ce.get_check())
+        ce.get_check()
         sys.exit(0)
     except Exception as err:
         print("main:: {err}".format(err=err))
